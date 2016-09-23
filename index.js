3c0568774e4bfcddf720afa81e15fc43c2f48083
Denzel Wamburu <denzjoseph@gmail.com>
2016-09-24 00:41:22 +0300

    Hello world

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-'use strict';
-
-var TelegramBot = require('node-telegram-bot-api');
-
-var TOKEN = '276973160:AAGi_I4vVdU3D86zn37kydA3Awars9BMYuM';
-var USER = 'USER_ID';
-
-var bot = new TelegramBot(TOKEN, {polling: {timeout: 1, interval: 100}});
-
-var opts = {
-  reply_markup: JSON.stringify(
-                            {
-                                  force_reply: true
-                                }
-                                  )};
-
-bot.sendMessage(USER, 'How old are you?', opts)
-  .then(function (sended) {
-      var chatId = sended.chat.id;
-      var messageId = sended.message_id;
-      bot.onReplyToMessage(chatId, messageId, function (message) {
-            console.log('User is %s years old', message.text);
-          });
+var TelegramBot = require('node-telegram-bot-api'),
+    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+    telegram = new TelegramBot("276973160:AAGi_I4vVdU3D86zn37kydA3Awars9BMYuM", {
+        polling: true
     });
+var request = require('request');
+telegram.on("text", (message) => {
+            telegram.sendMessage(message.chat.id, "The Mess is Below You!");
+          });
