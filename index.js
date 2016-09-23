c71f72cf0550071aa5367e5aa28f4cffece066b0
Denzel Wamburu <denzjoseph@gmail.com>
2016-09-24 01:22:39 +0300

    Add Request and Parse JSON

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ var TelegramBot = require('node-telegram-bot-api'),
         polling: true
     });
 var request = require('request');
+var foods;
+var options = {
+    method: 'GET',
+    json: true,
+    uri: 'http://localhost:3000/api/notices'
+}
+request(options, function(error, response, body) {
+    if (error)
+        console.log(error);
+    // else console.log(body[0]);
+    foods = body[0];
+    console.log(foods);
+});
+
 telegram.on("text", (message) => {
-            telegram.sendMessage(message.chat.id, "The Mess is Below You!");
-          });
+    // console.log(foods);
+    telegram.sendMessage(message.chat.id, "We are Working!");
+    // get message and respond
+    if (message.text.toLowerCase().indexOf("/lunch") === 0) {
+        // console.log(foods);
+        var lunchlist = foods.title
+        telegram.sendMessage(message.chat.id, "The Menu For Lunch is *" + lunchlist + "*!", {
+          parse_mode: "Markdown"
+
+        });
+
+    };
+    if (message.text.toLowerCase().indexOf("/breakfast") === 0) {
+        telegram.sendMessage(message.chat.id, "We are Working on something for Breakfast");
+
+    };
+    if (message.text.toLowerCase().indexOf("/supper") === 0) {
+        telegram.sendMessage(message.chat.id, "We are Working on something for Supper");
+
+    };
+
+});
