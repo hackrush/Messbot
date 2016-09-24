795165fc8325caa6e609f1de174ac3ede738d604
Denzel Wamburu <denzjoseph@gmail.com>
2016-09-24 05:52:45 +0300

    Fixed Vegsteaw typo

--- index.js
+++ index.js
@@ -22,40 +22,41 @@ request(options, function(error, response, body) {
     supper = body[2];
     console.log(supper);
 });
-
+// do cron to check for updates
+//
 telegram.on("text", (message) => {
         // console.log(foods);
         // get message and respond
         if (message.text.toLowerCase().indexOf("breakfast") === 0) {
-            var _list = breakfast;
+            var breakfast_list = breakfast;
             delete _list.type;
             delete _list.id;
             // var list = JSON.stringify(_list);
             var tea;
             console.log(_list);
             //status for tea
-            if (_list.tea == true) {
+            if (breakfast_list.tea == true) {
                 tea = "Available"
             } else {
                 tea = "Not Available"
             }
             // status for coffee
-            if (_list.coffee == true) {
+            if (breakfast_list.coffee == true) {
                 coffee = "Available"
             } else {
                 coffee = "Not Available"
             }
-            if (_list.cake == true) {
+            if (breakfast_list.cake == true) {
                 cake = "Available"
             } else {
                 cake = "Not Available"
             }
-            if (_list.eggs == true) {
+            if (breakfast_list.eggs == true) {
                 eggs = "Available"
             } else {
                 eggs = "Not Available"
             }
-            if (_list.sandwich == true) {
+            if (breakfast_list.sandwich == true) {
                 sandwich = "Available"
             } else {
                 sandwich = "Not Available"
@@ -71,43 +72,44 @@ telegram.on("text", (message) => {
 
         };
         if (message.text.toLowerCase().indexOf("lunch") === 0) {
-            var _list = lunch;
-            delete _list.type;
-            if (_list.rice == true) {
+            var lunch_list = lunch;
+            console.log(lunch_list);
+            delete lunch_list.type;
+            if (lunch_list.rice == true) {
                 rice = "Available"
             } else {
                 rice = "Not Available"
             }
             // status for coffee
-            if (_list.ugali == true) {
+            if (lunch_list.ugali == true) {
                 ugali = "Available"
             } else {
                 ugali = "Not Available"
             }
-            if (_list.tea == true) {
+            if (lunch_list.tea == true) {
                 cake = "Available"
             } else {
                 tea = "Not Available"
             }
-            if (_list.cabbage == true) {
+            if (lunch_list.cabbage == true) {
                 cabbage = "Available"
             } else {
                 cabbage = "Not Available"
             }
-            if (_list.greengrams == true) {
+            if (lunch_list.greengrams == true) {
                 greengrams = "Available"
             } else {
                 greengrams = "Not Available"
             }
-            if (_list.beans == true) {
+            if (lunch_list.beans == true) {
                 beans = "Available"
             } else {
                 beans = "Not Available"
             }
-            if (_list.vegstew == true) {
-                vegstew = "Available"
+            if (lunch_list.vegsteaw == true) {
+                vegsteaw = "Available"
             } else {
-                vegstew = "Not Available"
+                vegsteaw = "Not Available"
             }
             telegram.sendMessage(message.chat.id, "Lunch Menu: " + " *Rice* is " + "*" + rice + "*!" +
                 " *Ugali* is " + "*" + ugali + "*!" +
@@ -115,51 +117,51 @@ telegram.on("text", (message) => {
                 " *Greengrams* is " + "*" + greengrams + "*!" +
                 " *Cabbage* is " + "*" + cabbage + "*!" +
                 " *Tea* is " + "*" + tea + "*!" +
-                " *VegStew* is " + "*" + vegstew + "*!" +
+                " *VegStew* is " + "*" + vegsteaw + "*!" +
 
                 " ", {
                     parse_mode: "Markdown"
                 });
         };
         if (message.text.toLowerCase().indexOf("supper") === 0) {
-            var _list = supper;
-            delete _list.type;
-
-            if (_list.rice == true) {
+            var supper_list = supper;
+            delete supper_list.type;
+            console.log(supper_list);
+            if (supper_list.rice == true) {
                 rice = "Available"
             } else {
                 rice = "Not Available"
             }
             // status for coffee
-            if (_list.ugali == true) {
+            if (supper_list.ugali == true) {
                 ugali = "Available"
             } else {
                 ugali = "Not Available"
             }
-            if (_list.tea == true) {
+            if (supper_list.tea == true) {
                 tea = "Available"
             } else {
                 tea = "Not Available"
             }
-            if (_list.cabbage == true) {
+            if (supper_list.cabbage == true) {
                 cabbage = "Available"
             } else {
                 cabbage = "Not Available"
             }
-            if (_list.greengrams == true) {
+            if (supper_list.greengrams == true) {
                 greengrams = "Available"
             } else {
                 greengrams = "Not Available"
             }
-            if (_list.beans == true) {
+            if (supper_list.beans == true) {
                 beans = "Available"
             } else {
                 beans = "Not Available"
             }
-            if (_list.vegstew == true) {
-                vegstew = "Available"
+            if (supper_list.vegsteaw == true) {
+                vegsteaw = "Available"
             } else {
-                vegstew = "Not Available"
+                vegsteaw = "Not Available"
             }
             telegram.sendMessage(message.chat.id, "Supper Menu: " + " *Rice* is " + "*" + rice + "*!" +
                 " *Ugali* is " + "*" + ugali + "*!" +
@@ -167,7 +169,7 @@ telegram.on("text", (message) => {
                 " *Greengrams* is " + "*" + greengrams + "*!" +
                 " *Cabbage* is " + "*" + cabbage + "*!" +
                 " *Tea* is " + "*" + tea + "*!" +
-                " *VegStew* is " + "*" + vegstew + "*!" +
+                " *VegStew* is " + "*" + vegsteaw + "*!" +
 
                 " ", {
                     parse_mode: "Markdown"
@@ -198,155 +200,155 @@ telegram.on("text", (message) => {
             var _time = getGreetingTime(moment());
             // console.log(_time);
             if (_time == 'morning') {
-              var _list = breakfast;
-              delete _list.type;
-              delete _list.id;
-              // var list = JSON.stringify(_list);
-              var tea;
-              console.log(_list);
-              //status for tea
-              if (_list.tea == true) {
-                  tea = "Available"
-              } else {
-                  tea = "Not Available"
-              }
-              // status for coffee
-              if (_list.coffee == true) {
-                  coffee = "Available"
-              } else {
-                  coffee = "Not Available"
-              }
-              if (_list.cake == true) {
-                  cake = "Available"
-              } else {
-                  cake = "Not Available"
-              }
-              if (_list.eggs == true) {
-                  eggs = "Available"
-              } else {
-                  eggs = "Not Available"
-              }
-              if (_list.sandwich == true) {
-                  sandwich = "Available"
-              } else {
-                  sandwich = "Not Available"
-              }
-              telegram.sendMessage(message.chat.id, "Breakfast Menu: " + " *TEA* is " + "*" + tea + "*!" +
-                  " *Coffee* is " + "*" + coffee + "*!" +
-                  " *Cake* is " + "*" + cake + "*!" +
-                  " *Eggs* is " + "*" + eggs + "*!" +
-                  " *Sandwich* is " + "*" + sandwich + "*!" +
-                  "  ", {
-                      parse_mode: "Markdown"
-                  });
-            } else if (_time == 'afternoon' ) {
+                var _list = breakfast;
+                delete _list.type;
+                delete _list.id;
+                // var list = JSON.stringify(_list);
+                var tea;
+                console.log(_list);
+                //status for tea
+                if (_list.tea == true) {
+                    tea = "Available"
+                } else {
+                    tea = "Not Available"
+                }
+                // status for coffee
+                if (_list.coffee == true) {
+                    coffee = "Available"
+                } else {
+                    coffee = "Not Available"
+                }
+                if (_list.cake == true) {
+                    cake = "Available"
+                } else {
+                    cake = "Not Available"
+                }
+                if (_list.eggs == true) {
+                    eggs = "Available"
+                } else {
+                    eggs = "Not Available"
+                }
+                if (_list.sandwich == true) {
+                    sandwich = "Available"
+                } else {
+                    sandwich = "Not Available"
+                }
+                telegram.sendMessage(message.chat.id, "Breakfast Menu: " + " *TEA* is " + "*" + tea + "*!" +
+                    " *Coffee* is " + "*" + coffee + "*!" +
+                    " *Cake* is " + "*" + cake + "*!" +
+                    " *Eggs* is " + "*" + eggs + "*!" +
+                    " *Sandwich* is " + "*" + sandwich + "*!" +
+                    "  ", {
+                        parse_mode: "Markdown"
+                    });
+            } else if (_time == 'afternoon') {
 
-              var _list = lunch;
-              delete _list.type;
-              if (_list.rice == true) {
-                  rice = "Available"
-              } else {
-                  rice = "Not Available"
-              }
-              // status for coffee
-              if (_list.ugali == true) {
-                  ugali = "Available"
-              } else {
-                  ugali = "Not Available"
-              }
-              if (_list.tea == true) {
-                  cake = "Available"
-              } else {
-                  tea = "Not Available"
-              }
-              if (_list.cabbage == true) {
-                  cabbage = "Available"
-              } else {
-                  cabbage = "Not Available"
-              }
-              if (_list.greengrams == true) {
-                  greengrams = "Available"
-              } else {
-                  greengrams = "Not Available"
-              }
-              if (_list.beans == true) {
-                  beans = "Available"
-              } else {
-                  beans = "Not Available"
-              }
-              if (_list.vegstew == true) {
-                  vegstew = "Available"
-              } else {
-                  vegstew = "Not Available"
-              }
-              telegram.sendMessage(message.chat.id, "Lunch Menu: " + " *Rice* is " + "*" + rice + "*!" +
-                  " *Ugali* is " + "*" + ugali + "*!" +
-                  " *Beans* is " + "*" + beans + "*!" +
-                  " *Greengrams* is " + "*" + greengrams + "*!" +
-                  " *Cabbage* is " + "*" + cabbage + "*!" +
-                  " *Tea* is " + "*" + tea + "*!" +
-                  " *VegStew* is " + "*" + vegstew + "*!" +
+                var _list = lunch;
+                delete _list.type;
+                if (_list.rice == true) {
+                    rice = "Available"
+                } else {
+                    rice = "Not Available"
+                }
+                // status for coffee
+                if (_list.ugali == true) {
+                    ugali = "Available"
+                } else {
+                    ugali = "Not Available"
+                }
+                if (_list.tea == true) {
+                    cake = "Available"
+                } else {
+                    tea = "Not Available"
+                }
+                if (_list.cabbage == true) {
+                    cabbage = "Available"
+                } else {
+                    cabbage = "Not Available"
+                }
+                if (_list.greengrams == true) {
+                    greengrams = "Available"
+                } else {
+                    greengrams = "Not Available"
+                }
+                if (_list.beans == true) {
+                    beans = "Available"
+                } else {
+                    beans = "Not Available"
+                }
+                if (_list.vegsteaw == true) {
+                    vegsteaw = "Available"
+                } else {
+                    vegsteaw = "Not Available"
+                }
+                telegram.sendMessage(message.chat.id, "Lunch Menu: " + " *Rice* is " + "*" + rice + "*!" +
+                    " *Ugali* is " + "*" + ugali + "*!" +
+                    " *Beans* is " + "*" + beans + "*!" +
+                    " *Greengrams* is " + "*" + greengrams + "*!" +
+                    " *Cabbage* is " + "*" + cabbage + "*!" +
+                    " *Tea* is " + "*" + tea + "*!" +
+                    " *VegStew* is " + "*" + vegsteaw + "*!" +
 
-                  " ", {
-                      parse_mode: "Markdown"
-                  });
+                    " ", {
+                        parse_mode: "Markdown"
+                    });
             } else if (_time === 'evening') {
-              var _list = supper;
-              delete _list.type;
+                var _list = supper;
+                delete _list.type;
 
-              if (_list.rice == true) {
-                  rice = "Available"
-              } else {
-                  rice = "Not Available"
-              }
-              // status for coffee
-              if (_list.ugali == true) {
-                  ugali = "Available"
-              } else {
-                  ugali = "Not Available"
-              }
-              if (_list.tea == true) {
-                  tea = "Available"
-              } else {
-                  tea = "Not Available"
-              }
-              if (_list.cabbage == true) {
-                  cabbage = "Available"
-              } else {
-                  cabbage = "Not Available"
-              }
-              if (_list.greengrams == true) {
-                  greengrams = "Available"
-              } else {
-                  greengrams = "Not Available"
-              }
-              if (_list.beans == true) {
-                  beans = "Available"
-              } else {
-                  beans = "Not Available"
-              }
-              if (_list.vegstew == true) {
-                  vegstew = "Available"
-              } else {
-                  vegstew = "Not Available"
-              }
-              telegram.sendMessage(message.chat.id, "Supper Menu: " + " *Rice* is " + "*" + rice + "*!" +
-                  " *Ugali* is " + "*" + ugali + "*!" +
-                  " *Beans* is " + "*" + beans + "*!" +
-                  " *Greengrams* is " + "*" + greengrams + "*!" +
-                  " *Cabbage* is " + "*" + cabbage + "*!" +
-                  " *Tea* is " + "*" + tea + "*!" +
-                  " *VegStew* is " + "*" + vegstew + "*!" +
+                if (_list.rice == true) {
+                    rice = "Available"
+                } else {
+                    rice = "Not Available"
+                }
+                // status for coffee
+                if (_list.ugali == true) {
+                    ugali = "Available"
+                } else {
+                    ugali = "Not Available"
+                }
+                if (_list.tea == true) {
+                    tea = "Available"
+                } else {
+                    tea = "Not Available"
+                }
+                if (_list.cabbage == true) {
+                    cabbage = "Available"
+                } else {
+                    cabbage = "Not Available"
+                }
+                if (_list.greengrams == true) {
+                    greengrams = "Available"
+                } else {
+                    greengrams = "Not Available"
+                }
+                if (_list.beans == true) {
+                    beans = "Available"
+                } else {
+                    beans = "Not Available"
+                }
+                if (_list.vegsteaw == true) {
+                    vegsteaw = "Available"
+                } else {
+                    vegsteaw = "Not Available"
+                }
+                telegram.sendMessage(message.chat.id, "Supper Menu: " + " *Rice* is " + "*" + rice + "*!" +
+                    " *Ugali* is " + "*" + ugali + "*!" +
+                    " *Beans* is " + "*" + beans + "*!" +
+                    " *Greengrams* is " + "*" + greengrams + "*!" +
+                    " *Cabbage* is " + "*" + cabbage + "*!" +
+                    " *Tea* is " + "*" + tea + "*!" +
+                    " *VegStew* is " + "*" + vegsteaw + "*!" +
 
-                  " ", {
-                      parse_mode: "Markdown"
-                  });
+                    " ", {
+                        parse_mode: "Markdown"
+                    });
 
             } else {
-              telegram.sendMessage(message.chat.id, "I Didn't Understand you, kindly try *BreakFast*, *Lunch* or *Supper*", {
-                  parse_mode: "Markdown"
+                telegram.sendMessage(message.chat.id, "I Didn't Understand you, kindly try *BreakFast*, *Lunch* or *Supper*", {
+                    parse_mode: "Markdown"
 
-              });
+                });
             }
 
         }
